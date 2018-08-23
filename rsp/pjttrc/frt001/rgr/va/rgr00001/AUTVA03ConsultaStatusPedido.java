494ee505e84a24060cd61632d070465da34e8e4f
edinei-lopes <edinei.lopes@softtek.com>
2018-08-23 16:44:25 -0300

    Consulta Status do VA - Edinei

--- rsp/pjttrc/frt001/rgr/va/rgr00001/AUTVA03ConsultaStatusPedido.java
+++ rsp/pjttrc/frt001/rgr/va/rgr00001/AUTVA03ConsultaStatusPedido.java
@@ -3,6 +3,19 @@
  */
 package br.lry.qa.rsp.pjttrc.frt001.rgr.va.rgr00001;
 
+import com.borland.silktest.jtf.Desktop;
+import org.junit.Test;
+import com.borland.silktest.jtf.xbrowser.DomElement;
+import com.borland.silktest.jtf.xbrowser.DomLink;
+import com.borland.silktest.jtf.xbrowser.DomTextField;
+
+import br.lry.components.AUTBaseComponent;
+import br.lry.components.va.AUTVALogin;
+
+import com.borland.silktest.jtf.xbrowser.DomButton;
+import com.borland.silktest.jtf.xbrowser.DomRadioButton;
+import org.junit.Assert;
+
 /**
  * 
  * Objetivo principal :
@@ -14,7 +27,25 @@ package br.lry.qa.rsp.pjttrc.frt001.rgr.va.rgr00001;
  * @author Softtek - QA
  *
  */
-public class AUTVA03ConsultaStatusPedido {
-	public AUTVA03ConsultaStatusPedido (){}
+public class AUTVA03ConsultaStatusPedido extends AUTVALogin{
+
+	private Desktop desktop = new Desktop();
+
+	@Test
+	public void AUTVA03ConsultaStatusPedido() {
+
+		//start recording 
+		desktop.<DomElement>find("VA.TelaInicialLoja.BotaoFecharAprovacaoComercial").click();
+		desktop.<DomElement>find("VA.TelaInicialLoja.BotaoRecuperarCarrinho").click();
+		desktop.<DomElement>find("VA.PesquisarCarrinhos.BotaoFiltroCarrinho").click();
+		desktop.<DomElement>find("VA.PesquisarCarrinhos.OpcoesDeFiltro").click();
+		desktop.<DomTextField>find("VA.PesquisarCarrinhos.OpcoesDeFiltro.InformacoesDeFiltro").setText("0000218387");
+		desktop.<DomButton>find("VA.PesquisarCarrinhos.OpcoesDeFiltro.Buscar").click();
+		DomElement emEdição = desktop.<DomElement>find("VA.CadastroClientesDados.Em edição");
+		Assert.assertEquals("Em edição", emEdição.getText());
+		//end recording
+
+	}
+	
 
 }
