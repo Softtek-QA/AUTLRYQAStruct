f46644f33d4e75703e19047986aabe558b9c7955
fernandaaguiar <fernanda.aguiar@softtek.com>
2018-09-03 11:16:46 -0300

    update

---
+++ rsp/pjttrc/frt001/rgr/va/rgr00000/AUTSprint1.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package br.lry.qa.rsp.pjttrc.frt001.rgr.va.rgr00000;
+
+import org.junit.Test;
+import org.junit.internal.runners.TestMethod;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.borland.silktest.jtf.internal.JunitUtil;
+import com.borland.silktest.silk4j.playback.TestResult;
+import com.borland.silktest.silk4j.playback.TestRunResultProcessor;
+
+import br.lry.components.va.AUTVACadastros;
+import br.lry.components.va.AUTVACadastros.AUT_VA_TESTS_OPTIONS;
+import br.lry.qa.rsp.pjttrc.frt001.rgr.va.rgr00001.AUTVA01CadastroCliente;
+
+
+/**
+ * 
+ *Regressivo de testes integrados - Vendas Assistidas
+ * 
+ * @author Softtek-QA
+ *
+ */
+public class AUTSprint1 {	
+	
+	
+	
+	/**
+	 * 
+	 * Construtor padrão da classe
+	 * 
+	 */
+	public AUTSprint1() {
+		super();
+	}
+}
---
+++ rsp/pjttrc/frt001/rgr/va/rgr00000/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Administrador
+ *
+ */
+package br.lry.qa.rsp.pjttrc.frt001.rgr.va.rgr00000;
