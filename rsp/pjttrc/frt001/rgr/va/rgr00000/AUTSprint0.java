8bd946c4f907c3492d7803892c158e1fd3977057
marcosoliveirastk <marcos.oliveira@softtek.com>
2018-09-04 04:24:06 -0300

    Arquivos de configuração temporários do projeto

---
+++ rsp/pjttrc/frt001/rgr/va/rgr00000/AUTSprint0.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package br.lry.qa.rsp.pjttrc.frt001.rgr.va.rgr00000;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.internal.runners.TestMethod;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.junit.experimental.categories.*;
+import com.borland.silktest.jtf.internal.JunitUtil;
+import com.borland.silktest.silk4j.playback.TestResult;
+import com.borland.silktest.silk4j.playback.TestRunResultProcessor;
+
+import br.lry.components.va.AUTVACadastros;
+import br.lry.components.va.AUTVACadastros.AUT_VA_CADASTROS;
+import br.lry.dataflow.AUTDataFlow.AUT_TABLE_PARAMETERS_NAMES;
+import br.lry.components.va.AUTVAGeradorPedido;
+import br.lry.qa.rsp.pjttrc.frt001.rgr.va.rgr00001.AUTVA01CadastroCliente;
+
+
+/**
+ * 
+ *Regressivo de testes integrados - Vendas Assistidas
+ * 
+ * @author Softtek-QA
+ *
+ */
+@RunWith(com.borland.silktest.jtf.SilkTestSuite.class)
+@SuiteClasses(AUTVACadastros.class)
+public class AUTSprint0{	
+
+}
--- rsp/pjttrc/frt001/rgr/va/rgr00000/AUTSprint1.java
+++ rsp/pjttrc/frt001/rgr/va/rgr00000/AUTSprint1.java
@@ -3,17 +3,26 @@
  */
 package br.lry.qa.rsp.pjttrc.frt001.rgr.va.rgr00000;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.internal.runners.TestMethod;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.junit.runners.Suite;
-
+import org.junit.runners.Suite.SuiteClasses;
+import org.junit.experimental.categories.*;
 import com.borland.silktest.jtf.internal.JunitUtil;
 import com.borland.silktest.silk4j.playback.TestResult;
 import com.borland.silktest.silk4j.playback.TestRunResultProcessor;
 
 import br.lry.components.va.AUTVACadastros;
+import br.lry.components.va.AUTVACadastros.AUT_VA_CADASTROS;
+import br.lry.dataflow.AUTDataFlow.AUT_TABLE_PARAMETERS_NAMES;
+import br.lry.components.va.AUTVAGeradorPedido;
 import br.lry.qa.rsp.pjttrc.frt001.rgr.va.rgr00001.AUTVA01CadastroCliente;
 
 
@@ -24,9 +33,131 @@ import br.lry.qa.rsp.pjttrc.frt001.rgr.va.rgr00001.AUTVA01CadastroCliente;
  * @author Softtek-QA
  *
  */
-public class AUTSprint1 {	
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class AUTSprint1{	
+	public static AUTVACadastros cadastros = null; //Define a suite de testes focados em cadastros de clientes nas lojas
+	public static AUTVAGeradorPedido pedidos = null; //Define a suite de testes para criação de pedidos	
+	public static String USUARIO_GLOBAL = "00000000",SENHA_GLOBAL="1234";
+	
+	/**
+	 * 
+	 * Retorna o gerenciador de componentes automatizados para módulo de cadastro
+	 * 
+	 * @return AUTVACadastros - Objeto para gerenciamento de cadastros
+	 * 
+	 */
+	public AUTVACadastros autGetIncludeClientManagement() {
+		return new AUTVACadastros();
+	}
 	
 	
+	/**
+	 * 
+	 * Retorna o gerenciador de componentes automatizados de pedidos
+	 * 
+	 * @return AUTVAGeradorPedido - Gerenciador de pedidos
+	 * 
+	 */
+	public AUTVAGeradorPedido autGetRequestsManagement() {		
+		pedidos = new AUTVAGeradorPedido(USUARIO_GLOBAL,SENHA_GLOBAL);
+		return pedidos;
+	}
+	
+	@Test
+	public void AUT_CT000_CADASTROS_CONFIGURACAO() {
+		cadastros = autGetIncludeClientManagement();
+	}
+	/**
+	 * 
+	 * Executa os procedimentos para cadastro de cliente - Pessoa Física
+	 * 
+	 * 
+	 */
+	@Test
+	public void AUT_CT001_CADASTRO_PESSOA_FISICA() {
+		cadastros.autInitClientMenuCadastroPF();
+	}
+	
+	
+	/**
+	 * 
+	 * Executa os procedimentos para cadastro de cliente - Pessoa Estrangeira
+	 * 
+	 * 
+	 */
+	//@Test
+	public void AUT_CT002_CADASTRO_PESSOA_ESTRANGEIRO() {
+		cadastros.autInitClientMenuCadastroExtrangeiro();
+	}
+
+	/**
+	 * 
+	 * Executa os procedimentos para cadastro de cliente - Pessoa Jurídica
+	 * 
+	 */
+	//@Test
+	public void AUT_CT003_CADASTRO_PESSOA_JURIDICA() {
+		cadastros.autInitClientMenuCadastroPJ();
+	}
+
+
+	@Test
+	public void AUT_CT004_CONFIGURACAO_PARAMETROS_PEDIDOS() {
+		USUARIO_GLOBAL = autGetIncludeClientManagement().autGetCurrentParameter(AUT_TABLE_PARAMETERS_NAMES.AUT_VA_LOGIN,"AUT_USER").toString();
+		SENHA_GLOBAL = autGetIncludeClientManagement().autGetCurrentParameter(AUT_TABLE_PARAMETERS_NAMES.AUT_VA_LOGIN,"AUT_PASSWORD").toString();
+		pedidos = autGetRequestsManagement();		
+		pedidos.AUT_CLIENT_DOC_CPF = cadastros.autGetCurrentParameter(AUT_TABLE_PARAMETERS_NAMES.AUT_VA_CADASTROS,"AUT_CPF").toString();
+		pedidos.AUT_CLIENT_DOC_CNPJ = cadastros.autGetCurrentParameter(AUT_TABLE_PARAMETERS_NAMES.AUT_VA_CADASTROS,"AUT_CNPJ").toString();
+		pedidos.AUT_CLIENT_DOC_PASSAPORT = cadastros.autGetCurrentParameter(AUT_TABLE_PARAMETERS_NAMES.AUT_VA_CADASTROS,"AUT_PASSAPORTE").toString();	
+		pedidos.AUT_VA_USER = USUARIO_GLOBAL;
+		pedidos.AUT_VA_PASSWORD = SENHA_GLOBAL;
+		pedidos.AUT_CLIENT_TYPE = AUT_VA_CADASTROS.FISICA;			
+	}
+	
+	@Test
+	public void AUT_CT005_GERAR_PEDIDO_CAIXA_PAG_DINHEIRO_CLIENT_PF() {
+		pedidos.autCaixaPagDinheiro();
+	}
+
+	@Test
+	public void AUT_CT006_GERAR_PEDIDO_CAIXA_PAG_CARTAO_CLIENT_PF() {
+		pedidos.autCaixaPagCartaoCredito();
+	}
+
+	@Test
+	public void AUT_CT007_GERAR_PEDIDO_CAIXA_PAG_CARTAO_CELEBRE_CLIENT_PF() {
+		pedidos.autCaixaPagCartaoCelebre();
+	}
+
+	@Test
+	public void AUT_CT008_GERAR_PEDIDO_RET_INTERNA_IMED_PAG_CARTAO_CLIENT_PF() {
+		pedidos.autRetiraInternaImediataPagCartaoCredito();
+	}
+
+	@Test
+	public void AUT_CT009_GERAR_PEDIDO_RET_INTERNA_IMED_PAG_CARTAO_CELEBRE_CLIENT_PF() {
+		pedidos.autRetiraInternaImediataPagCartaoCelebre();
+	}
+	
+	@Test
+	public void AUT_CT010_GERAR_PEDIDO_RET_INTERNA_IMED_PAG_DINHEIRO_CLIENT_PF() {
+		pedidos.autRetiraInternaImediataPagDinheiro();
+	}
+
+	@Test
+	public void AUT_CT011_GERAR_PEDIDO_RET_EXTERNA_IMED_PAG_CARTAO_CLIENT_PF() {
+		pedidos.autRetiraExternaImediataPagCartaoCredito();
+	}
+
+	@Test
+	public void AUT_CT012_GERAR_PEDIDO_RET_EXTERNA_IMED_PAG_CARTAO_CELEBRE_CLIENT_PF() {
+		pedidos.autRetiraExternaImediataPagCartaoCelebre();
+	}
+	
+	@Test
+	public void AUT_CT013_GERAR_PEDIDO_RET_EXTERNA_IMED_PAG_DINHEIRO_CLIENT_PF() {
+		pedidos.autRetiraExternaImediataPagDinheiro();
+	}
 	
 	/**
 	 * 
@@ -35,5 +166,6 @@ public class AUTSprint1 {
 	 */
 	public AUTSprint1() {
 		super();
+		
 	}
 }
