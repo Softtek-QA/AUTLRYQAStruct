3d7117d88963e311be13261c7630e42033dec7d6
Administrador <Administrador@AUTVMLRY00005>
2018-10-24 09:01:30 -0300

    Cenários 001 e 018

--- rsp/pjttrc/frt001/va/md00004/cn00001/CTP00001.java
+++ rsp/pjttrc/frt001/va/md00004/cn00001/CTP00001.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import org.junit.Test;
 
 import br.lry.components.AUTVABaseComponent;
+import br.lry.components.safe.AUTSafeLogin;
 import br.lry.dataflow.AUTDataFlow;
 import br.lry.dataflow.AUTDataFlow.AUT_TABLE_PARAMETERS_NAMES;
 
@@ -17,12 +18,14 @@ import br.lry.dataflow.AUTDataFlow.AUT_TABLE_PARAMETERS_NAMES;
  */
 public class CTP00001 extends  AUTVABaseComponent {
 	
-	
+	@Test
 	public void cn0001() {
 		 autGetDataFlow().autInitDataFlow();
-		 CMP00001(autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00004_CN00001_CTP00001));
-		 CMP00036(autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00004_CN00001_CTP00001));
-		 CMP00022();
+		 AUTSafeLogin safe = new AUTSafeLogin();
+		 safe.autStartLoginDefault("123", "123");
+//		 CMP00001(autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00004_CN00001_CTP00001));
+//		 CMP00036(autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00004_CN00001_CTP00001));
+//		 CMP00022();
 	}
 
 }
