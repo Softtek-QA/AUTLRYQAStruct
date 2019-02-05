48054817a824b2681be52eb6fb044d79a5a0badb
Administrador <Administrador@AUTVMLRY00009>
2019-02-04 22:03:53 -0200

    ENTREGAS 6-10 - SINCRONIZAÇÃO DE CORREÇÃO FRAMEWORK

--- rsp/pjttrc/frt001/va/md00001/cn00070/CTP00001.java
+++ rsp/pjttrc/frt001/va/md00001/cn00070/CTP00001.java
@@ -3,14 +3,16 @@ package br.lry.qa.rsp.pjttrc.frt001.va.md00001.cn00070;
 import org.junit.Test;
 
 import com.borland.silktest.jtf.xbrowser.DomButton;
+import com.borland.silktest.jtf.xbrowser.DomCheckBox;
 import com.borland.silktest.jtf.xbrowser.DomElement;
+import com.borland.silktest.jtf.xbrowser.DomListBox;
 import com.borland.silktest.jtf.xbrowser.DomRadioButton;
-import com.borland.silktest.jtf.xbrowser.DomTextField;
 import br.lry.components.AUTVABaseComponent;
 import br.lry.dataflow.AUTDataFlow.AUT_TABLE_PARAMETERS_NAMES;
 
-import com.borland.silktest.jtf.xbrowser.BrowserApplication;
+
 import com.borland.silktest.jtf.xbrowser.BrowserWindow;
+
 
 /**
  * 
@@ -46,24 +48,20 @@ public class CTP00001 extends AUTVABaseComponent{
 				AUT_AGENT_SILK4J.<DomElement>find("VA.FluxoSaida.PopUpFreteAdicional").click();
 				autInsertScreenByScenario();
 			}
-			/**
-		 WebDriver driver = AUT_AGENT_SILK4J.<BrowserApplication>find("//BrowserApplication").getWebDriver();
-		 JavascriptExecutor jse = (JavascriptExecutor) driver;
-		 jse.executeScript("window.scrollBy(0,500)", "");
-		 AUT_AGENT_SILK4J.<BrowserWindow>find("//BrowserApplication").executeJavaScript(code);
 		 
-		 driver.findElement(By.id("itementrega-0")).click();
-		 new Select(driver.findElement(By.id("deliveryOptionType-0"))).selectByIndex(1);
-		 new Select(driver.findElement(By.id("deliveryOptionDeposit-0"))).selectByVisibleText(autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00001_CN00070_CTP00001,"AUT_FILIAL_ESTOQUE").toString());
-		 driver.findElement(By.id("nearDate-0-delivery")).click();
-		 new Select(driver.findElement(By.id("deliveryOptionShift-0"))).selectByIndex(1);
-		 driver.findElement(By.id("itementrega-1")).click();
-		 new Select(driver.findElement(By.id("deliveryOptionType-1"))).selectByIndex(1);
-		 new Select(driver.findElement(By.id("deliveryOptionDeposit-1"))).selectByVisibleText(autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00001_CN00070_CTP00001,"AUT_FILIAL_ESTOQUE").toString());
-		 driver.findElement(By.id("nearDate-1-delivery")).click();
-		 new Select(driver.findElement(By.id("deliveryOptionShift-1"))).selectByIndex(1);
-		 **/
-		 AUT_AGENT_SILK4J.<DomButton>find("VA.AtualizacaoDados.Avançar").click();		 
+			AUT_AGENT_SILK4J.<DomRadioButton>find("VA.AtualizacaoDados.//INPUT[@id='itementrega-0']").click();		
+			AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//SELECT[@id='deliveryOptionType-0']").select(1);
+			AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//SELECT[@id='deliveryOptionDeposit-0']").select(1);
+			AUT_AGENT_SILK4J.<DomCheckBox>find("VA.AtualizacaoDados.//INPUT[@id='nearDate-0-delivery']").click();
+			AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//SELECT[@id='deliveryOptionShift-0']").select(2);
+
+			AUT_AGENT_SILK4J.<DomRadioButton>find("VA.AtualizacaoDados.//INPUT[@id='itementrega-1']").click();		
+			AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//SELECT[@id='deliveryOptionType-1']").select(1);
+			AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//SELECT[@id='deliveryOptionDeposit-1']").select(1);
+			AUT_AGENT_SILK4J.<DomCheckBox>find("VA.AtualizacaoDados.//INPUT[@id='nearDate-1-delivery']").click();
+			AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//SELECT[@id='deliveryOptionShift-1']").select(1);
+
+		 AUT_AGENT_SILK4J.<DomButton>find("VA.AtualizacaoDados.Avançar").click();
 		 CMP00020(autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00001_CN00019_CTP00001)).autSelecaoMeioPagamento(autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00001_CN00070_CTP00001));
 		 AUT_AGENT_SILK4J.<DomButton>find("VA.AtualizacaoDados.Avançar").click();
 		 CMP00034(autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00001_CN00019_CTP00001));	
--- rsp/pjttrc/frt001/va/md00001/cn00071/CTP00001.java
+++ rsp/pjttrc/frt001/va/md00001/cn00071/CTP00001.java
@@ -3,13 +3,16 @@ package br.lry.qa.rsp.pjttrc.frt001.va.md00001.cn00071;
 import org.junit.Test;
 
 import com.borland.silktest.jtf.xbrowser.DomButton;
+import com.borland.silktest.jtf.xbrowser.DomCheckBox;
 import com.borland.silktest.jtf.xbrowser.DomElement;
+import com.borland.silktest.jtf.xbrowser.DomListBox;
 import com.borland.silktest.jtf.xbrowser.DomRadioButton;
-import com.borland.silktest.jtf.xbrowser.DomTextField;
 import br.lry.components.AUTVABaseComponent;
 import br.lry.dataflow.AUTDataFlow.AUT_TABLE_PARAMETERS_NAMES;
-import com.borland.silktest.jtf.xbrowser.BrowserApplication;
+
+
 import com.borland.silktest.jtf.xbrowser.BrowserWindow;
+
 
 /**
  * 
@@ -45,25 +48,22 @@ public class CTP00001 extends AUTVABaseComponent{
 				AUT_AGENT_SILK4J.<DomElement>find("VA.FluxoSaida.PopUpFreteAdicional").click();
 				autInsertScreenByScenario();
 			}
-			/**
-		 WebDriver driver = AUT_AGENT_SILK4J.<BrowserApplication>find("//BrowserApplication").getWebDriver();
-		 JavascriptExecutor jse = (JavascriptExecutor) driver;
-		 jse.executeScript("window.scrollBy(0,500)", "");
-		 driver.findElement(By.id("itementrega-0")).click();
-		 new Select(driver.findElement(By.id("deliveryOptionType-0"))).selectByIndex(1);
-		 new Select(driver.findElement(By.id("deliveryOptionDeposit-0"))).selectByVisibleText(autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00001_CN00070_CTP00001,"AUT_FILIAL_ESTOQUE").toString());
-		 driver.findElement(By.id("nearDate-0-delivery")).click();
-		 new Select(driver.findElement(By.id("deliveryOptionShift-0"))).selectByIndex(1);
-		 driver.findElement(By.id("itementrega-1")).click();
-		 new Select(driver.findElement(By.id("deliveryOptionType-1"))).selectByIndex(2);
-		 new Select(driver.findElement(By.id("deliveryOptionDeposit-1"))).selectByVisibleText(autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00001_CN00070_CTP00001,"AUT_FILIAL_ESTOQUE").toString());
-		 driver.findElement(By.id("nearDate-1-delivery")).click();
-		 new Select(driver.findElement(By.id("deliveryOptionShift-1"))).selectByIndex(1);
-		 **/
+			AUT_AGENT_SILK4J.<DomRadioButton>find("VA.AtualizacaoDados.//INPUT[@id='itementrega-0']").click();		
+			AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//SELECT[@id='deliveryOptionType-0']").select(1);
+			AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//SELECT[@id='deliveryOptionDeposit-0']").select(1);
+			AUT_AGENT_SILK4J.<DomCheckBox>find("VA.AtualizacaoDados.//INPUT[@id='nearDate-0-delivery']").click();
+			AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//SELECT[@id='deliveryOptionShift-0']").select(2);
+
+			AUT_AGENT_SILK4J.<DomRadioButton>find("VA.AtualizacaoDados.//INPUT[@id='itementrega-1']").click();		
+			AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//SELECT[@id='deliveryOptionType-1']").select(2);
+			AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//SELECT[@id='deliveryOptionDeposit-1']").select(1);
+			AUT_AGENT_SILK4J.<DomCheckBox>find("VA.AtualizacaoDados.//INPUT[@id='nearDate-1-delivery']").click();
+			AUT_AGENT_SILK4J.<DomListBox>find("VA.AtualizacaoDados.//SELECT[@id='deliveryOptionShift-1']").select(1);
 		 AUT_AGENT_SILK4J.<DomButton>find("VA.AtualizacaoDados.Avançar").click();		 
 		 CMP00020(autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00001_CN00071_CTP00001)).autSelecaoMeioPagamento(autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00001_CN00071_CTP00001));
 		 AUT_AGENT_SILK4J.<DomButton>find("VA.AtualizacaoDados.Avançar").click();
 		 CMP00034(autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00001_CN00071_CTP00001));	
 		 CMP00022();		
+
 	}
 }
