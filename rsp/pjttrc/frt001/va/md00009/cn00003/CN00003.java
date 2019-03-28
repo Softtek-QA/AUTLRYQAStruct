d0a6fd1418d7f8a4ac4eb2f5dba2cd81911b84df
Administrador <Administrador@AUTVMLRY00009>
2019-03-28 06:20:37 -0300

    INCLUSAO DE CENARIOS - COMPLEXIDADE ALTA

---
+++ rsp/pjttrc/frt001/va/md00009/cn00003/CN00003.java
@@ -0,0 +1,41 @@
+package br.lry.qa.rsp.pjttrc.frt001.va.md00009.cn00003;
+
+import org.junit.Test;
+
+import com.borland.silktest.jtf.xbrowser.BrowserWindow;
+
+import br.lry.components.AUTVABaseComponent;
+
+public class CN00003 extends AUTVABaseComponent {
+
+	/**
+	 * 
+	 * Inicia o fluxo de execução padrão do cenário
+	 * 
+	 * @return boolean - True caso o processo seja finalizado com sucesso, false caso contrário
+	 * 
+	 */
+	@Test
+	public void autStartProcess() {
+		try {
+			System.out.println("AUT INFO: START LOGIN PROCESS");
+			br.lry.qa.rsp.pjttrc.frt001.va.md00009.cn00001.CTP00001 ct1 = new br.lry.qa.rsp.pjttrc.frt001.va.md00009.cn00001.CTP00001();			
+			ct1.AUT_IT00003_STVA_ID00004_FRT004_CN00001_GERACAO_PEDIDOS();				
+		}
+		catch(java.lang.Exception e) {
+			System.out.println("AUT ERROR: START LOGIN PROCESS");
+			System.out.println(e.getMessage());
+			e.printStackTrace();					
+		}
+	}
+	
+	
+	/**
+	 * Construtor
+	 * 
+	 */
+	public CN00003() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
