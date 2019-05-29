4c2678667600b2b75ad08b668e13108c3d789da3
alan.mattos <amtt@softtek.com>
2019-05-29 14:13:29 -0300

    Entrega 2 nova

--- rsp/pjttrc/frt001/va/md00000/cn00003/CTP00001.java
+++ rsp/pjttrc/frt001/va/md00000/cn00003/CTP00001.java
@@ -27,25 +27,18 @@ public class CTP00001 extends AUTVABaseComponent {
 	 * @return
 	 */
 	@Test
-	public void autCadastroEstrangeiro() {
+	public void autCadastro() {
 
 		autGetDataFlow().autInitDataFlow();
 		java.util.HashMap<String, Object> parametros = new java.util.HashMap<String, Object>();
-		parametros = autGetDataFlow()
-				.autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00000_CN00001_CTR00001);
+		parametros = autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_VA_MD00000_CN00003_CTP00001);
 
-		autGetDataFlow().autInitDataFlow();
 		CMP00001(parametros);
 
 		
-		//start recording 
-		AUT_AGENT_SILK4J.<DomElement>find("VA.AtualizacaoDados.FechaJanelaComentario").click();
-		AUT_AGENT_SILK4J.<DomElement>find("VA.AtualizacaoDados.menu-trigger relativ").click();
-		AUT_AGENT_SILK4J.<DomElement>find("VA.TelaInicialLoja.SubMenuClientes").click();
-		AUT_AGENT_SILK4J.<DomLink>find("VA.CadastroClientesDados.AdicionarNovo").click();
-		//end recording
+		CMP00066();
 		
-		CMP00072(parametros);
+		CMP00067(parametros);
 		//CMP00022();
 
 	}
