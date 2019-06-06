ef8a0c89320f5499cb2968d4353ed13c2e871808
alan.mattos <amtt@softtek.com>
2019-06-06 09:55:37 -0300

    Alteração CN06 Nao Sei CEP

--- rsp/pjttrc/frt001/va/md00000/cn00009/CN00009.java
+++ rsp/pjttrc/frt001/va/md00000/cn00009/CN00009.java
@@ -10,14 +10,16 @@ public class CN00009 extends AUTVABaseComponent{
 
 	/**
 	 * 
-	 * CN00005 - Realizar um cadastro de um cliente Estrangeiro
-	 * Agrupamento de todos os casos de testes do CN00005
+	 * CN00009 - Realizar cadastro de cliente utilizando "Não sei meu cep"
+	 * Agrupamento de todos os casos de testes do CN00009
 	 * @param parametro - Parametros de entrada do sistema
 	 * @return
 	 */
 	
 	@Test
 	public void autStartProcess09() {
-			CMP00066();
+		br.lry.qa.rsp.pjttrc.frt001.va.md00000.cn00009.CTP00001 ctp1 = new br.lry.qa.rsp.pjttrc.frt001.va.md00000.cn00009.CTP00001();	
+		ctp1.autCadastroClienteSemCEP();
+	
 	}
 }
