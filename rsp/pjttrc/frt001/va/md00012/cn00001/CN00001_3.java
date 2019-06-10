a6e7cf00061ddc075fddf9810f74065fcdb12f80
vanessa-medeiros <vanessa.medeiros@softtek.com>
2019-06-10 11:05:48 -0300

    Ajustes para o SAP

--- rsp/pjttrc/frt001/va/md00012/cn00001/CN00001_3.java
+++ rsp/pjttrc/frt001/va/md00012/cn00001/CN00001_3.java
@@ -12,11 +12,11 @@ public class CN00001_3 {
 		br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00001.CTP00005 ctp5 = new br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00001.CTP00005();	
 		br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00001.CTP00006 ctp6 = new br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00001.CTP00006();
 			
-		ctp5.FaturaPedidoSAP(numPedido); //FATURA_PEDIDO_ESTRANGEIRO
+		ctp5.FaturaPedidoSAP(numPedido); //FATURA_PEDIDO_PF
 		
 		ctp6.AUT_NUMERO_PEDIDO=ctp5.AUT_NUMERO_PEDIDO;	
 		ctp6.AUT_STATUS_ESPERADO=ctp5.AUT_STATUS_ESPERADO;
 			
-		ctp6.ValidaStatusVA(); //VALIDA_STATUS_PEDIDO_FATURADO_ESTRANGEIRO;
+		//ctp6.ValidaStatusVA(); //VALIDA_STATUS_PEDIDO_FATURADO_PF;
 	}
 }
