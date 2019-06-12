99a8a28dcfc1d72683e2e22b2f87cebde9897a93
vanessa-medeiros <vanessa.medeiros@softtek.com>
2019-06-12 10:29:09 -0300

    Ajustes para cenario 01 e 02 do SAP - Entrega 01

--- rsp/pjttrc/frt001/va/md00012/cn00002/CN00002_3.java
+++ rsp/pjttrc/frt001/va/md00012/cn00002/CN00002_3.java
@@ -10,7 +10,7 @@ import br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00002.CTP00006;
 public class CN00002_3 {
 	
 	@Test
-	public void autStartProcess01(String numPedido) {
+	public void autStartProcess02(String numPedido) {
 		br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00002.CTP00005 ctp5 = new br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00002.CTP00005();	
 		br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00002.CTP00006 ctp6 = new br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00002.CTP00006();
 			
