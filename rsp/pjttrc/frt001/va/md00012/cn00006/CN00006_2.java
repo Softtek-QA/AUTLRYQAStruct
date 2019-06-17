7bbfb3299f670c2cd3c6ae268f7dc35248482503
carlos.akamatsu <carlos.akamatsu@softtek.com>
2019-06-17 08:34:51 -0300

    Ajustes para CN006

--- rsp/pjttrc/frt001/va/md00012/cn00006/CN00006_2.java
+++ rsp/pjttrc/frt001/va/md00012/cn00006/CN00006_2.java
@@ -8,15 +8,16 @@ import br.lry.dataflow.AUTDataFlow.AUT_TABLE_PARAMETERS_NAMES;
 public class CN00006_2 {
 	
 	@Test
-	public void autStartProcess01(String numPedido) {
-		br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00001.CTP00003 ctp3 = new br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00001.CTP00003();	
-		br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00001.CTP00004 ctp4 = new br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00001.CTP00004();
+	public void autStartProcess06(String numPedido) {
+		br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00006.CTP00003 ctp3 = new br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00006.CTP00003();	
+		br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00006.CTP00004 ctp4 = new br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00006.CTP00004();
 			
-		ctp3.PagaPedidoPDV(numPedido); //PAGAMENTO_PEDIDO_PF;
+		ctp3.PagaPedidoPDV(numPedido); //PAGAMENTO_PEDIDO_ESTRANJEIRO;
 	
 		ctp4.AUT_NUMERO_PEDIDO=ctp3.AUT_NUMERO_PEDIDO;	
 		ctp4.AUT_STATUS_ESPERADO=ctp3.AUT_STATUS_ESPERADO;
 			
-		ctp4.ValidaStatusVA(); //VALIDA_STATUS_PEDIDO_PAGO_PF;
+		ctp4.ValidaStatusVA(); //VALIDA_STATUS_PEDIDO_PAGO_ESTRANJEIRO;
 	}
+
 }
