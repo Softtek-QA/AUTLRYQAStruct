56eb9fb2a7ece4525d4512328dfe299608462ff0
carlos.akamatsu <carlos.akamatsu@softtek.com>
2019-06-25 15:36:54 -0300

    Modificações e inclusões  para geração de vouchers cenários 11, 12 e 13

---
+++ rsp/pjttrc/frt001/va/md00012/cn00012/CN00012.java
@@ -0,0 +1,17 @@
+package br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00012;
+
+import org.junit.Test;
+
+import br.lry.components.AUTVABaseComponent;
+import br.lry.dataflow.AUTDataFlow.AUT_TABLE_PARAMETERS_NAMES;
+
+public class CN00012{
+
+	
+	@Test
+	public void autStartProcess12() {
+		br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00012.CTP00001 ctp1 = new br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00012.CTP00001();	
+		
+		ctp1.GeraVoucherEst(); //VALIDA_VALE_TROCA_ESTRANGEIRA;
+	}
+}
---
+++ rsp/pjttrc/frt001/va/md00012/cn00012/CTP00001.java
@@ -0,0 +1,48 @@
+package br.lry.qa.rsp.pjttrc.frt001.va.md00012.cn00012;
+
+import java.util.HashMap;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.borland.silktest.jtf.win32.AccessibleControl;
+import com.borland.silktest.jtf.xbrowser.DomButton;
+import com.borland.silktest.jtf.xbrowser.DomCheckBox;
+import com.borland.silktest.jtf.xbrowser.DomElement;
+import com.borland.silktest.jtf.xbrowser.DomLink;
+import com.borland.silktest.jtf.xbrowser.DomListBox;
+import com.borland.silktest.jtf.xbrowser.DomRadioButton;
+import com.borland.silktest.jtf.xbrowser.DomTextField;
+
+import br.lry.components.AUTSafeBaseComponent;
+import br.lry.components.AUTVABaseComponent;
+import br.lry.dataflow.AUTDataFlow.AUT_TABLE_PARAMETERS_NAMES;
+
+public class CTP00001 extends AUTSafeBaseComponent {
+	
+	public static String AUT_NUMERO_PEDIDO;
+	public static String AUT_STATUS_ESPERADO;
+	
+	/**
+	 * 
+	 * 
+	 *  
+	 * parametro - Parametros de entrada do sistema
+	 * @param
+	 * @return
+	 */
+	@Test
+	public void GeraVoucherEst() {
+
+		autGetDataFlow().autInitDataFlow();
+		java.util.HashMap<String, Object> parametros = new java.util.HashMap<String, Object>();
+		parametros = autGetDataFlow().autGetParametersFromTable(AUT_TABLE_PARAMETERS_NAMES.RSP_PJTTRC_FRT001_SAFE_MD00012_CN00012_CTP00001);
+		
+		CMP00093(parametros);
+		
+		CMP00098(parametros).autIniCadastroClienteConveniadoEst(parametros);
+		
+		CMP00095();
+
+	}
+}
